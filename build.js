b15608ed94db6e1cca42f766be39c4c32a1f25d7
eliaslohem-eng <eliaslohem@gmail.com>
2026-07-04 15:19:30 -0400

    Update build.js

--- build.js
+++ build.js
@@ -1,15 +1,22 @@
 import fs from 'fs';
 import path from 'path';
+
 fs.rmSync('dist', { recursive: true, force: true });
 fs.mkdirSync('dist', { recursive: true });
-function copy(src, dest) {
-  const stat = fs.statSync(src);
-  if (stat.isDirectory()) {
-    fs.mkdirSync(dest, { recursive: true });
-    for (const item of fs.readdirSync(src)) copy(path.join(src, item), path.join(dest, item));
-  } else {
-    fs.copyFileSync(src, dest);
+
+const archivos = [
+  'index.html',
+  'manifest.json',
+  'sw.js',
+  'icon-192.png',
+  'icon-512.png',
+  'icon-1024.png'
+];
+
+for (const archivo of archivos) {
+  if (fs.existsSync(archivo)) {
+    fs.copyFileSync(archivo, path.join('dist', archivo));
   }
 }
-copy('public', 'dist');
+
 console.log('Build listo en dist/');
